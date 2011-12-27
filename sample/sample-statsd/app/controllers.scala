3d820b83cbb9c5f99cd51836a0eb323e30123699
David Y. Ross <dyross@gmail.com>
2011-12-27 09:42:10 -0800

    making sample controller compile in eclipse easily, adding a bit to docs

--- sample/sample-statsd/app/controllers.scala
+++ sample/sample-statsd/app/controllers.scala
@@ -4,11 +4,5 @@ import play._
 import play.mvc._
 
 object Application extends Controller {
-    
-    import views.Application._
-    
-    def index = {
-        html.index("Your Scala application is ready!")
-    }
-    
+  def index = "it worked"
 }
